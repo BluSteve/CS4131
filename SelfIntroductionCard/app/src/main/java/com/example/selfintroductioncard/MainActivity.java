31987ee6564fd2a61523ce258307e9906c5580b1
Steve Cao <billiegatesftw@gmail.com>
2020-01-16 14:52:55 +0800

    Added bottomnavigationview for SelfIntroductionCard

--- SelfIntroductionCard/app/src/main/java/com/example/selfintroductioncard/MainActivity.java
+++ SelfIntroductionCard/app/src/main/java/com/example/selfintroductioncard/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.selfintroductioncard;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActionBar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Basic Information");
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_basicinfo:
+                    toolbar.setTitle("Basic Information");
+                    return true;
+                case R.id.navigation_history:
+                    toolbar.setTitle("My Life Story");
+                    return true;
+                case R.id.navigation_hobbies:
+                    toolbar.setTitle("Hobbies");
+                    return true;
+                case R.id.navigation_phone:
+                    toolbar.setTitle("Call me!");
+                    return true;
+            }
+            return false;
+        }
+    };
 }
