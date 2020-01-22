0fc99e6e9b0c8b7f9262739577bd9391601147d8
Steve Cao <billiegatesftw@gmail.com>
2020-01-22 11:59:44 +0800

    Added call me button in SelfIntroductionCard

--- SelfIntroductionCard/app/src/main/java/com/example/selfintroductioncard/MainActivity.java
+++ SelfIntroductionCard/app/src/main/java/com/example/selfintroductioncard/MainActivity.java
@@ -4,14 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import android.content.Intent;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private ActionBar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("Basic Information");
     }
 
+    private void hideInfo() {
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setVisibility(View.INVISIBLE);
+    }
+    private void showInfo() {
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setVisibility(View.VISIBLE);
+    }
+
+    private void showCallMe() {
+        Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "(+65)96312156"));
+        Button button = findViewById(R.id.callme);
+        button.setOnClickListener((s) -> {
+            startActivity(i);
+        });
+    }
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -33,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
             Fragment fragment;
             switch (item.getItemId()) {
                 case R.id.navigation_basicinfo:
+                    showCallMe();
+                    showInfo();
                     toolbar.setTitle("Basic Information");
                     return true;
                 case R.id.navigation_history:
@@ -42,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                     toolbar.setTitle("Hobbies");
                     return true;
                 case R.id.navigation_phone:
+                    hideInfo();
                     toolbar.setTitle("Call me!");
                     return true;
             }
